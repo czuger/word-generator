44ac7440331e735260e86c60dcd424e0f4356b9f
ced <zuger.cedric@gmail.com>
2019-11-13 18:35:02 +0100

    First word generation test. Not really good.

---
+++ word_generation.rb
@@ -0,0 +1,41 @@
+require 'json'
+
+letters_matrix = JSON.parse( File.read( 'words_db/fr' + '/letters_matrix.json' ) )
+
+# Generate 10 words
+1.upto( 10 ).each do
+
+	word_array = []
+	current_letter = letters_matrix.keys.sample
+	word_array << current_letter
+
+	1.upto( rand( 4 .. 8 ) ).each do
+		l_m = letters_matrix[current_letter]
+
+		break unless l_m
+
+		max_rand = l_m['max_rand']
+		r = rand( 0 .. max_rand )
+
+		l_m['letter_rand_max'].keys.reverse.each do |k|
+
+			# puts "#{r}, #{k}, #{l_m['letter_rand_max'][k]}"
+
+			if r < k.to_i
+				# Nothing
+			else
+				new_letter = l_m['letter_rand_max'][k]
+				word_array << new_letter
+				current_letter = new_letter
+
+				# p current_letter
+
+				break
+			end
+		end
+	end
+
+	puts word_array.join
+	puts
+
+end
