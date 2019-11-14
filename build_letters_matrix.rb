63265a76be782a1f211ce2a1515121d713aade80
ced <zuger.cedric@gmail.com>
2019-11-14 11:56:42 +0100

    First letter is also random now.

--- build_letters_matrix.rb
+++ build_letters_matrix.rb
@@ -1,5 +1,20 @@
 require 'json'
 
+def count_hash_to_statistic_array(array )
+	result = []
+
+	total_counts = array.map{ |k, v| v }.reduce( &:+ ).to_f
+
+	array.each do |corresponding_letter, letter_count|
+		stat = letter_count * 100 / total_counts
+		if stat > 0.01
+			result << [ stat, corresponding_letter ]
+		end
+	end
+
+	result
+end
+
 Dir['words_db/*'].each do |locale|
 	if File.directory?( locale )
 
@@ -51,21 +66,11 @@ Dir['words_db/*'].each do |locale|
 		letters_matrix.keys.each do |key|
 			lm = letters_matrix[key]
 
-			total_counts = lm[:letters_counts].map{ |k, v| v }.reduce( &:+ ).to_f
-
-			lm[:letters_counts].each do |corresponding_letter, letter_count|
-
-				lm[ :letter_statistics ] ||= []
-
-				stat = letter_count * 100 / total_counts
-				if stat > 0.05
-					lm[ :letter_statistics ] << [ stat, corresponding_letter ]
-				end
-
-			end
-
+			lm[:letter_statistics] = count_hash_to_statistic_array(lm[:letters_counts] )
 			lm.delete( :letters_counts )
 		end
+
+		first_letters = count_hash_to_statistic_array(first_letters )
 
 		File.open( locale + '/letters_matrix.json', 'w' ) do |f|
 			f.write JSON.pretty_generate(
